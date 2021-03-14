64ca71cd5b86c708f9f111434fb604a08fe45d33
bjorn <bjorn@swis.nl>
2021-03-14 20:19:17 +0100

    Initial custom ContentEntity for timestamps to replace the custom database table.

--- README.md
+++ README.md
@@ -9,6 +9,25 @@
 * Possibly implement something like EntityReferenceSelection where you can set the valid entity types for a plugin. This makes installing a specific plugin for other modules easier.
 * Tests? ;)
 
+* Create "timestamp" type for schema_metatag
+  * First implementation is a full list of fields. But perhaps
+```json
+{
+  "timestamp": {
+    "@type": "BlockchainTransaction",
+    "identifier": "bb93587586f089f83526eb90f93f3951c11b328bf81c492e18ba162a84cea1b0",
+    "hash": "0d869841dccdabf8b25a727412023b3159c23c585e298b23317a0d05ad4164fa",
+    "hashLink": "https://www.nrc.nl/api/wordproof/hashinput?id=4339608",
+    "recordedIn": {
+      "@type": "Blockchain",
+      "name": "eosio_main"
+    }
+  }
+}
+```
+
+
+
 * Wordproof -> WordProof
 
 ## Done
---
+++ src/Entity/Timestamp.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Drupal\wordproof\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+
+/**
+ * Defines the Timestamp entity.
+ *
+ * @ingroup timestamp
+ *
+ * @ContentEntityType(
+ *   id = "timestamp",
+ *   label = @Translation("Timestamp"),
+ *   base_table = "timestamp",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "uuid" = "uuid",
+ *     "id" = "id",
+ *     "uuid" = "uuid",
+ *     "entity_id" = "entity_id",
+ *     "revision_id" = "revision_id",
+ *     "hash" = "hash",
+ *     "transaction_blockchain" = "transaction_blockchain",
+ *     "transaction_id" = "transaction_id",
+ *     "transaction_link" = "transaction_link",
+ *     "hashinput_text" = "hashinput_text",
+ *     "date_created" = "date_created",
+ *   }
+ * )
+ */
+class Timestamp extends ContentEntityBase implements ContentEntityInterface {
+
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    // Standard field, used as unique if primary index.
+    $fields['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('ID'))
+      ->setDescription(t('The ID of the Advertiser entity.'))
+      ->setReadOnly(TRUE);
+
+    // Standard field, unique outside of the scope of the current project.
+    $fields['uuid'] = BaseFieldDefinition::create('uuid')
+      ->setLabel(t('UUID'))
+      ->setDescription(t('The UUID of the Advertiser entity.'))
+      ->setReadOnly(TRUE);
+
+    // Standard field, unique outside of the scope of the current project.
+    $fields['entity_id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('The entity id'))
+      ->setDescription(t('The referred entity id for the timestamp.'));
+
+    // Standard field, unique outside of the scope of the current project.
+    $fields['revision_id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('The revision id'))
+      ->setDescription(t('The referred revision id for the referred entity.'));
+
+    // Standard field, unique outside of the scope of the current project.
+    $fields['hash'] = BaseFieldDefinition::create('char')
+      ->setLabel(t('The revision id'))
+      ->setSettings(
+        [
+          'length' => 64,
+        ]
+      )
+      ->setDescription(t('Hash of the HashInput of this content.'));
+
+    // Standard field, unique outside of the scope of the current project.
+    $fields['transaction_blockchain'] = BaseFieldDefinition::create('char')
+      ->setLabel(t('Blockchain used'))
+      ->setSettings(
+        [
+          'length' => 128,
+        ]
+      )
+      ->setDescription(t('Blockchain used to record the content.'));
+
+    // Standard field, unique outside of the scope of the current project.
+    $fields['transaction_id'] = BaseFieldDefinition::create('char')
+      ->setLabel(t('Blockchain transaction id'))
+      ->setSettings(
+        [
+          'length' => 128,
+        ]
+      )
+      ->setDescription(t('Blockchain transaction to record the content.'));
+
+    // Standard field, unique outside of the scope of the current project.
+    $fields['transaction_link'] = BaseFieldDefinition::create('char')
+      ->setLabel(t('Blockchain transaction link'))
+      ->setSettings(
+        [
+          'length' => 128,
+        ]
+      )
+      ->setDescription(t('Link to blockchain transaction.'));
+
+    // Standard field, unique outside of the scope of the current project.
+    $fields['hashinput_text'] = BaseFieldDefinition::create('text')
+      ->setLabel(t('HashInput '))
+      ->setSettings(
+        [
+          'size' => 'medium',
+        ]
+      )
+      ->setDescription(t('The HashInput on which the hash is based.'));
+
+    // Standard field, unique outside of the scope of the current project.
+    $fields['date_created'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Unix timestamp '))
+      ->setDefaultValue(0)
+      ->setDescription(t('The HashInput on which the hash is based.'));
+
+    return $fields;
+  }
+
+}
