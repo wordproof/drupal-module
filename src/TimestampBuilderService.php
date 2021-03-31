db5f6b3fe585f58ab8a22bcb62e9ddd5ccbc4c4f
bjorn <bjorn@swis.nl>
2021-03-31 16:40:11 +0200

    Implement stamping through entitiy reference. For example: stamp a node when a references taxonomy is changed.

--- src/TimestampBuilderService.php
+++ src/TimestampBuilderService.php
@@ -5,6 +5,7 @@ namespace Drupal\wordproof;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityPublishedInterface;
+use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\wordproof\Exception\InvalidEntityException;
 use Drupal\wordproof\Plugin\BlockchainBackendInterface;
 use Drupal\wordproof\Plugin\BlockchainBackendManager;
@@ -32,6 +33,11 @@ class TimestampBuilderService {
    * @var \Drupal\Core\Config\ImmutableConfig
    */
   private $config;
+
+  /**
+   * @var array
+   */
+  private $watchList;
 
 
   public function __construct(StamperManager $stamperManager, BlockchainBackendManager $blockchainBackendManager, TimestampRepositoryInterface $timestampRepository, ConfigFactoryInterface $configFactory) {
@@ -99,5 +105,56 @@ class TimestampBuilderService {
     $isEnabled = $this->config->get('stamper.' . $entity->getEntityTypeId() . '-' . $entity->bundle() . '.enabled');
     return $isEnabled === '1';
   }
+
+
+  private function buildWatchList(): array {
+    if (isset($this->watchList)) {
+      return $this->watchList;
+    }
+
+    /** @var FieldStorageConfig[] $entityReferenceFields */
+    $entityReferenceFields = \Drupal::service('entity_type.manager')
+      ->getStorage('field_storage_config')
+      ->loadByProperties(['type' => 'entity_reference']);
+    $watchList = [];
+    foreach ($entityReferenceFields as $field) {
+      $target_type = $field->getSetting('target_type');
+      if (!isset($watchList[$target_type])) {
+        $watchList[$target_type] = [];
+      }
+      $targetEntityTypeId = $field->getTargetEntityTypeId();
+      if (!isset($watchList[$target_type])) {
+        $watchList[$target_type][$targetEntityTypeId] = [];
+      }
+
+      $watchList[$target_type][$targetEntityTypeId][] = $field->getName();
+    }
+    return $this->watchList = $watchList;
+  }
+
+  public function stampWatchedEntities(ContentEntityInterface $entity) {
+    $watchList = $this->buildWatchList();
+    $entityTypeId = $entity->getEntityTypeId();
+
+    if (isset($watchList[$entityTypeId])) {
+      foreach ($watchList[$entityTypeId] as $targetEntityTypeId => $fields) {
+        $query = \Drupal::entityQuery($targetEntityTypeId, 'OR');
+        foreach ($fields as $field_name) {
+          $query->condition($field_name, $entity->id(), 'IN');
+        }
+        $result = $query->execute();
+
+        if (count($result) > 0) {
+          $entities = \Drupal::entityTypeManager()->getStorage($targetEntityTypeId)->loadMultiple($result);
+          foreach ($entities as $entity) {
+            if($entity instanceof ContentEntityInterface){
+              $this->stamp($entity);
+            }
+          }
+        }
+      }
+    }
+  }
+
 
 }
