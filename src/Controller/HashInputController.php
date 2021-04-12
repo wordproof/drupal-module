b9dfff4248719d52e28d1d1431e0587c683c4bdb
bjorn <bjorn@swis.nl>
2021-04-13 00:21:20 +0200

    By default get revisions in HashInput controller

--- src/Controller/HashInputController.php
+++ src/Controller/HashInputController.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class HashInputController {
   public function get($id) {
-    $hashInput = \Drupal::service('wordproof.repository')->getHashInput($id);
+    $hashInput = \Drupal::service('wordproof.repository')->getHashInput($id, TRUE);
     return new JsonResponse($hashInput, 200, [], true);
   }
 }
