996dc8922f75d79399395a83ebe313447cadc694
bjorn <bjorn@swis.nl>
2021-04-30 13:31:52 +0200

    Add test backend with some placeholders.

---
+++ src/Plugin/wordproof_timestamp/BlockchainBackend/TestBackend.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\wordproof_timestamp\Plugin\wordproof_timestamp\BlockchainBackend;
+
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Queue\QueueFactory;
+use Drupal\wordproof_timestamp\Plugin\BlockchainBackendInterface;
+use Drupal\wordproof_timestamp\Timestamp\TimestampInterface;
+use Drupal\wordproof_timestamp\WordProofApiClientInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines an blockchain backend implementation for WordProof.
+ *
+ * @BlockchainBackend(
+ *   id = "wordproof_test_backend",
+ *   title = @Translation("WordProof API Queued Blockchain backend"),
+ *   description = @Translation("Blockchain backend for WordProof API create hashes on a blockchain. Uses the queued checks to call the API for blockchain info instead of a WebHook")
+ * )
+ */
+class TestBackend implements BlockchainBackendInterface {
+
+  public function send(TimestampInterface $timestamp): TimestampInterface {
+    $timestamp->setHashInput('hashinput');
+    $timestamp->setHash(hash('sha256', $timestamp->getContent()));
+    $timestamp->setRemoteId($timestamp->id());
+    $timestamp->setTransactionAddress(md5($timestamp->id()));
+    $timestamp->setTransactionBlockchain('eos');
+    $timestamp->setTransactionId(md5($timestamp->id()));
+    $timestamp->setTransactionLink('https://testlink.com');
+    return $timestamp;
+  }
+
+}
