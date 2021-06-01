8ff838304d3c44d47e9b5bdf2ec5d89856280fa5
bjorn <bjorn@swis.nl>
2021-06-01 14:26:52 +0200

    Add revision configuration to certificate component so it doesn't show revision buttons when revisions are not enabled.

--- src/Plugin/Block/WordProofCertificateBlock.php
+++ src/Plugin/Block/WordProofCertificateBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\wordproof\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\wordproof\TimestampRepositoryInterface;
@@ -32,22 +33,31 @@ class WordProofCertificateBlock extends BlockBase implements ContainerFactoryPlu
    */
   private $repository;
 
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, TimestampRepositoryInterface $repository) {
+  /**
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  private $config;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, TimestampRepositoryInterface $repository, ConfigFactoryInterface $configFactory) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityTypeManager = $entity_type_manager;
     $this->repository = $repository;
+
+    $this->config = $configFactory->get('wordproof.settings');
   }
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+
     return new static(
       $configuration,
       $plugin_id,
       $plugin_definition,
       $container->get('entity_type.manager'),
-      $container->get('wordproof.repository')
+      $container->get('wordproof.repository'),
+      $container->get('config.factory')
     );
   }
 
@@ -66,6 +76,9 @@ class WordProofCertificateBlock extends BlockBase implements ContainerFactoryPlu
       return [
         '#theme' => 'wordproof_certificate',
         '#timestamp' => $timestamp,
+        '#settings' => [
+          'revisions' => (bool) $this->config->get('enable_revisions'),
+        ],
         '#attached' => [
           'library' => [
             'wordproof/certificate_module',
